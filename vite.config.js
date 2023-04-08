ffd809ddf555189bbb73e2e3e503b869fcdd14fb
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-04-08 21:51:25 +0530

    Added GIthub Actions Configrartion

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,
-  },
-  preview: {
-    port: 8080,
-  },
+  server: { port: 8080 },
+  preview: { port: 8080 },
+  base:'/rest-countries-api-with-color-theme-switcher/'
 });
