a8b81aa53522b897dbf3f095221ccfab96464622
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-04-08 13:11:36 +0530

    Added Server default config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+  },
+  preview: {
+    port: 8080,
+  },
+});
